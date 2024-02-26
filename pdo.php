562bd48cae55c3c7ed7fccd11b8db49511a3cba7
programarMusica <144407028+programarMusica@users.noreply.github.com>
2024-02-26 02:11:56 -0600

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,12 +1,12 @@
 <?php
-// Datos de conexión a la base de datos
-$servername = "127.0.0.1"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "mantecada"; 
+// Datos de conexiÃ³n a la base de datos
+$servername = "localhost"; 
+$username = "sajacolima"; 
+$password = "Colim@1234#@1234"; 
+$dbname = "i9781293_nluk1"; 
 
 try {
-    // Crear conexión PDO
+    // Crear conexiÃ³n PDO
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
     // Configurar el modo de error
@@ -16,7 +16,7 @@ try {
     $conn->exec("SET NAMES 'utf8'");
 } catch(PDOException $e) {
     // En caso de error, mostrar mensaje y terminar el script
-    die("Error de conexión: " . $e->getMessage());
+    die("Error de conexiÃ³n: " . $e->getMessage());
 }
 ?>
 
